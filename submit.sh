11183bdbf3db6b93204b01d8b52c1a4e9f8c001c
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-31 20:11:51 -0600

    exclude hidden files from zip

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm thx.unit.zip
-zip -r thx.unit.zip hxml src test doc/ImportUnit.hx extraParams.hxml haxelib.json LICENSE README.md
+zip -r thx.unit.zip hxml src test doc/ImportUnit.hx extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
 haxelib submit thx.unit.zip
