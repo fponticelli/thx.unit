605f3b1e12e556f181215baaa0274cf562ef8b81
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-10-15 23:11:34 -0600

    added tests combining thx.Time/DateTimeUtc and thx.unit.time.* units

--- test/thx/unit/TestTime.hx
+++ test/thx/unit/TestTime.hx
@@ -11,4 +11,19 @@ class TestTime {
       ((120 : Second) * 60).toHour() == (2 : Hour)
     );
   }
+
+  public function testDateDelta() {
+    var date : thx.DateTimeUtc = "2015-01-01 06:00:00",
+        hour : Hour = 3,
+        span : thx.unit.Time = "2h";
+
+    Assert.equals("2015-01-01T09:00:00Z", (date + hour).toString());
+    Assert.equals("2015-01-01T08:00:00Z", (date + span).toString());
+  }
+
+  public function testDateTime() {
+    var time : thx.Time = "03:00",
+        hour : Hour = time;
+    Assert.equals("3h", hour.toString());
+  }
 }
